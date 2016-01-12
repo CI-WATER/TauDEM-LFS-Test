cde6938bd7da243bcb7b273724a5410831e874d4
Pabitra Dash <pkdash_reena@hotmail.com>
2016-01-12 13:11:48 -0700

    added back folder RefrenceResult

---
+++ TestSuite/ReferenceResult/loganESRIGRID/info/arc0001.nit
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:37db15f9196cb41bd4b879e343fe4d598a852ccf22ce5f64b946bb16e4254118
+size 576
---
+++ TestSuite/ReferenceResult/testall.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e5b90db22e14ed6cdff6e72870e5eef8c869e62c23c886e576b704db3682d6bb
+size 15560
